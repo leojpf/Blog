0675bec519c05118ab64668cffa967f9090bcd6f
Leonardo Pita <leojpf@hotmail.com>
2019-10-22 21:09:24 -0300

    Adição da view Novo

--- Blog/Controllers/PostController.cs
+++ Blog/Controllers/PostController.cs
@@ -9,15 +9,28 @@ namespace Blog.Controllers
 {
     public class PostController : Controller
     {
-        public ViewResult Index()
+        List<PostViewModel> lista = new List<PostViewModel>();
+        public PostController()
         {
-            var lista = new List<PostViewModel>();
+            lista.Add(new PostViewModel("HP 1", "Resumo_HP1 ", " Categoria_HP1"));
+            lista.Add(new PostViewModel("HP 2", "Resumo_HP2 ", " Categoria_HP2"));
+            lista.Add(new PostViewModel("HP 3", "Resumo_HP3 ", " Categoria_HP3"));
+        }
 
-            lista.Add(new PostViewModel("HP 1", "Resumo_HP1", "Categoria_HP1"));
-            lista.Add(new PostViewModel("HP 2", "Resumo_HP2", "Categoria_HP2"));
-            lista.Add(new PostViewModel("HP 3", "Resumo_HP3", "Categoria_HP3"));
-
+        public IActionResult Index()
+        {
             return View(lista);
+        }
+
+        public IActionResult Novo()
+        {
+            return View();
+        }
+
+        public IActionResult Adiciona(string titulo, string resumo, string categoria)
+        {
+            lista.Add(new PostViewModel(titulo, resumo, categoria));
+            return View("Index", lista);
         }
     }
 }
