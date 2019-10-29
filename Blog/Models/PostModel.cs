32bfea8418fab059bebd0caf8a4b7aacf39a1572
Leonardo Pita <leojpf@hotmail.com>
2019-10-28 21:00:57 -0300

    Alteração no modelo para apresentação de prompt.

--- Blog/Models/PostModel.cs
+++ Blog/Models/PostModel.cs
@@ -7,11 +7,13 @@ namespace Blog.Models
     {
         public int Id { get; set; }
         [Required]
-        [StringLength(50, ErrorMessage = "Digite no máximo 50 caracteres")]
+        [StringLength(50, ErrorMessage = "Digite no máximo 50 caracteres")][Display(Name ="Título", Prompt ="Título")]
         public string Titulo { get; set; }
         [Required]
+        [Display(Name = "Categoria", Prompt = "Categoria")]
         public string Categoria { get; set; }
         [Required]
+        [Display(Name = "Resumo", Prompt = "Resumo")]
         public string Resumo { get; set; }
         public DateTime? Data_Publicacao { get; set; }
         public bool Publicado { get; set; }
