f8e812ff40e8c21bb984d916c1fcb3cf519837f5
Leonardo Pita <leojpf@hotmail.com>
2019-10-30 20:24:04 -0300

    Adição da home/index.

--- Blog/Controllers/HomeController.cs
+++ Blog/Controllers/HomeController.cs
@@ -5,14 +5,27 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Blog.Models;
+using Blog.DAO;
 
 namespace Blog.Controllers
 {
     public class HomeController : Controller
     {
+        PostDAONovo dao;
+        public HomeController(PostDAONovo Dao)
+        {
+            dao = Dao;
+        }
         public IActionResult Index()
         {
-            return View();
+            return View(dao.Listar().Where(p => p.Publicado).OrderByDescending(p => p.Data_Publicacao).ToList());
+        }
+        public IActionResult Buscar(string termo)
+        {
+            var retorno = dao.Listar().Where(p => (p.Publicado) && (p.Titulo.Contains(termo) || p.Resumo.Contains(termo)))
+                .Select(p => p)
+                .ToList();
+            return View("Index", retorno);
         }
 
         public IActionResult Privacy()
